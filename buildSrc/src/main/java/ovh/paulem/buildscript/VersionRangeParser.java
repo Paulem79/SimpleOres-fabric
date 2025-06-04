97210ca26112c90005edff4ee308ac51efab9fee
Paulem <66211574+Paulem79@users.noreply.github.com>
2025-06-04 22:02:13 +0200

    PUBLISH Unified publishing
    Updated to 1.21.6-pre3

--- buildSrc/src/main/java/ovh/paulem/buildscript/VersionRangeParser.java
+++ buildSrc/src/main/java/ovh/paulem/buildscript/VersionRangeParser.java
@@ -11,13 +11,15 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class VersionRangeParser {
 
     public static Iterable<? extends String> parseVersionRange(String min_version_range, String max_version_range) {
-        List<String> allVersions = getAllMinecraftVersions().reversed();
-        int startElement = allVersions.indexOf(min_version_range);
-        int endElement = allVersions.indexOf(max_version_range);
+        List<String> allVersions = getAllMinecraftVersions();
+
+        int startElement = allVersions.contains(min_version_range) ? allVersions.indexOf(min_version_range) : 0;
+        int endElement = !allVersions.contains(max_version_range) ? allVersions.size() - 1 : allVersions.indexOf(max_version_range);
 
         return allVersions.stream()
                 .filter(element -> allVersions.indexOf(element) >= startElement && allVersions.indexOf(element) <= endElement)
@@ -36,17 +38,20 @@ public class VersionRangeParser {
                 JsonObject json = gson.fromJson(reader, JsonObject.class);
                 JsonArray versionsArray = json.getAsJsonArray("versions");
 
-                for (int i = 0; i < versionsArray.size(); i++) {
-                    JsonObject versionObject = versionsArray.get(i).getAsJsonObject();
-                    String versionId = versionObject.get("id").getAsString();
-                    if (versionId.matches("1\\.\\d+(\\.\\d+)?")) { // Matches 1.x or 1.x.x
-                        versions.add(versionId);
-                    }
-                }
+                versions = revRange(0, versionsArray.size())
+                        .mapToObj(i -> versionsArray.get(i).getAsJsonObject()
+                                .get("id").getAsString()
+                        )
+                        .collect(Collectors.toList());
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
         return versions;
+    }
+
+    private static IntStream revRange(int from, int to) {
+        return IntStream.range(from, to)
+                .map(i -> to - i + from - 1);
     }
 }
