fa2453ed8dff5fdc7835af9dd981ab5b59d9edc3
Paulem <66211574+Paulem79@users.noreply.github.com>
2025-08-21 00:42:09 +0200

    fix(tint): fixed tint being incorrect

--- src/client/java/ovh/paulem/simpleores/tint/ChildrenBucketTintSourceAdapter.java
+++ src/client/java/ovh/paulem/simpleores/tint/ChildrenBucketTintSourceAdapter.java
@@ -1,17 +1,23 @@
 package ovh.paulem.simpleores.tint;
 
+import net.fabricmc.fabric.api.client.render.fluid.v1.FluidRenderHandler;
+import net.fabricmc.fabric.api.client.render.fluid.v1.FluidRenderHandlerRegistry;
 import net.minecraft.client.render.item.tint.TintSource;
+import net.minecraft.fluid.Fluid;
 import net.minecraft.fluid.Fluids;
+import net.minecraft.util.math.ColorHelper;
 import ovh.paulem.simpleores.items.custom.bucket.CustomChildrenBucketItem;
 
 public record ChildrenBucketTintSourceAdapter(CustomChildrenBucketItem bucket) {
     public TintSource getTintSource() {
-        if (bucket.getFluid() == Fluids.LAVA) {
-            return new ChildrenBucketTintSource(0xFF4500); // Lava color
-        } else if (bucket.getFluid() == Fluids.WATER) {
-            return new ChildrenBucketTintSource(0x3F76E4); // Water color
-        } else {
-            return new ChildrenBucketTintSource(0xFF0000);
+        Fluid fluid = bucket.getFluid();
+
+        int color = ColorHelper.withAlpha(255, 0xFFFFFF); // Default color is white
+        FluidRenderHandler fluidRenderHandler;
+        if (fluid != Fluids.EMPTY && (fluidRenderHandler = FluidRenderHandlerRegistry.INSTANCE.get(fluid)) != null) {
+            color = ColorHelper.withAlpha(255, fluidRenderHandler.getFluidColor(null, null, fluid.getDefaultState()));
         }
+
+        return new ChildrenBucketTintSource(color);
     }
 }
