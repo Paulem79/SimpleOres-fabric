40478216678fe1ae5fac277a9bed8af4f8b43f59
Paulem79 <66211574+Paulem79@users.noreply.github.com>
2026-03-10 22:29:37 +0100

    feat: updated to 26.1-snapshot-11

--- src/client/java/net/paulem/simpleores/mixin/GeneratedItemModelMixin.java
+++ src/client/java/net/paulem/simpleores/mixin/GeneratedItemModelMixin.java
@@ -1,15 +1,16 @@
-package net.paulem.simpleores.mixin;
-
-import org.spongepowered.asm.mixin.Mixin;
-
 //? if hasBucketlib || !containsBucket {
-/*@Mixin
+/*
+package net.paulem.simpleores.mixin;
+@Mixin
 public class GeneratedItemModelMixin {}
 *///?} else {
+package net.paulem.simpleores.mixin;
+
+import net.minecraft.client.resources.model.cuboid.ItemModelGenerator;
+import org.spongepowered.asm.mixin.Mixin;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import net.minecraft.client.data.models.model.TextureSlot;
-import net.minecraft.client.renderer.block.model.ItemModelGenerator;
 import org.objectweb.asm.Opcodes;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import org.spongepowered.asm.mixin.injection.At;
@@ -21,12 +22,21 @@ import java.util.List;
 @Mixin(ItemModelGenerator.class)
 public class GeneratedItemModelMixin {
     // Allow up to x layers in the model instead of 5 layers from vanilla
-    @WrapOperation(//? if >1.21.10 {
-            method = "bake(Lnet/minecraft/client/renderer/block/model/TextureSlots;Lnet/minecraft/client/resources/model/ModelBaker;Lnet/minecraft/client/resources/model/ModelState;Lnet/minecraft/client/resources/model/ModelDebugName;)Lnet/minecraft/client/resources/model/QuadCollection;",
+    @WrapOperation(
+            //? if afterDeobf {
+            method = "bake(Lnet/minecraft/client/resources/model/sprite/TextureSlots;Lnet/minecraft/client/resources/model/ModelBaker;Lnet/minecraft/client/renderer/block/dispatch/ModelState;Lnet/minecraft/client/resources/model/ModelDebugName;)Lnet/minecraft/client/resources/model/geometry/QuadCollection;",
+            //?} elif >1.21.10 {
+            //method = "bake(Lnet/minecraft/client/renderer/block/model/TextureSlots;Lnet/minecraft/client/resources/model/ModelBaker;Lnet/minecraft/client/resources/model/ModelState;Lnet/minecraft/client/resources/model/ModelDebugName;)Lnet/minecraft/client/resources/model/QuadCollection;",
             //?} else {
             /*method = "bake(Lnet/minecraft/client/renderer/block/model/TextureSlots;Lnet/minecraft/client/resources/model/SpriteGetter;Lnet/minecraft/client/resources/model/ModelState;Lnet/minecraft/client/resources/model/ModelDebugName;)Lnet/minecraft/client/resources/model/QuadCollection;",
             *///?}
-            at = @At(value = "FIELD", target = "Lnet/minecraft/client/renderer/block/model/ItemModelGenerator;LAYERS:Ljava/util/List;", opcode = Opcodes.GETSTATIC))
+            at = @At(value = "FIELD",
+                    //? if afterDeobf {
+                    target = "Lnet/minecraft/client/resources/model/cuboid/ItemModelGenerator;LAYERS:Ljava/util/List;",
+                    //?} else {
+                    //target = "Lnet/minecraft/client/renderer/block/model/ItemModelGenerator;LAYERS:Ljava/util/List;",
+                    //?}
+                    opcode = Opcodes.GETSTATIC))
     private static List<String> getLayers(Operation<List<String>> original) {
         return Arrays.stream(LayersUploader.LAYERS).map(TextureSlot::getId).toList();
     }
