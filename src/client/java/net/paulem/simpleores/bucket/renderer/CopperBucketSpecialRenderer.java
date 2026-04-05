346112f07b5e131f6bd102162b55075b75efb2bb
Paulem79 <66211574+Paulem79@users.noreply.github.com>
2026-04-05 18:42:02 +0200

    feat: advancing on new bucket render

--- src/client/java/net/paulem/simpleores/bucket/renderer/CopperBucketSpecialRenderer.java
+++ src/client/java/net/paulem/simpleores/bucket/renderer/CopperBucketSpecialRenderer.java
@@ -6,7 +6,6 @@ import com.mojang.serialization.MapCodec;
 import java.util.function.Consumer;
 
 import net.minecraft.client.model.geom.ModelLayerLocation;
-import net.minecraft.client.model.geom.ModelLayers;
 import net.minecraft.client.renderer.SubmitNodeCollector;
 import net.minecraft.client.renderer.rendertype.RenderType;
 import net.minecraft.client.renderer.rendertype.RenderTypes;
@@ -20,11 +19,14 @@ import net.paulem.simpleores.items.custom.bucket.CustomChildrenBucketItem;
 import net.paulem.simpleores.stonecutter.SCId;
 import org.joml.Vector3f;
 import org.joml.Vector3fc;
+import org.jspecify.annotations.NonNull;
 
 public class CopperBucketSpecialRenderer implements SpecialModelRenderer<Fluid> {
     public static final Transformation DEFAULT_TRANSFORMATION = new Transformation(null, null, new Vector3f(1.0F, -1.0F, -1.0F), null);
 
-    public static final Identifier COVER_TEXTURE = Identifier.fromNamespaceAndPath("simpleores", "textures/item/copper_bucket_cover.png");
+    public static final Identifier COVER_TEXTURE = SCId.of("textures/item/copper_bucket_cover.png");
+    private static final RenderType COVER_RENDER_TYPE = RenderTypes.itemCutout(COVER_TEXTURE);
+    public static final ModelLayerLocation COPPER_BUCKET_MODEL_LAYER = new ModelLayerLocation(SCId.of("copper_bucket"), "");
 
     private final CopperBucketModel model;
 
@@ -45,7 +47,7 @@ public class CopperBucketSpecialRenderer implements SpecialModelRenderer<Fluid> 
     @Override
     public void submit(
             final Fluid fluid,
-            final PoseStack poseStack,
+            final @NonNull PoseStack poseStack,
             final SubmitNodeCollector submitNodeCollector,
             final int lightCoords,
             final int overlayCoords,
@@ -54,27 +56,26 @@ public class CopperBucketSpecialRenderer implements SpecialModelRenderer<Fluid> 
     ) {
         System.out.println("Rendering Copper Bucket with fluid: " + fluid);
         // Render fluid
-        if (fluid != Fluids.EMPTY) {
+        //if (fluid != Fluids.EMPTY) {
             Identifier fluidTexture = getFluidTexture(fluid);
-            RenderType fluidRenderType = RenderTypes.entityCutout(fluidTexture);
+            RenderType fluidRenderType = RenderTypes.itemCutout(fluidTexture);
 
             submitNodeCollector.submitModelPart(
                     this.model.root(), poseStack, fluidRenderType, lightCoords, overlayCoords, null, false, false, -1, null, outlineColor
             );
-        }
+        //}
 
         // Render cover
-        RenderType coverRenderType = RenderTypes.entityCutout(COVER_TEXTURE);
         submitNodeCollector.submitModelPart(
-                this.model.root(), poseStack, coverRenderType, lightCoords, overlayCoords, null, false, hasFoil, -1, null, outlineColor
+                this.model.root(), poseStack, COVER_RENDER_TYPE, lightCoords, overlayCoords, null, false, hasFoil, -1, null, outlineColor
         );
     }
 
     private Identifier getFluidTexture(Fluid fluid) {
         // Retourne la texture du fluide. Dans un vrai mod, on chercherait l'Identifier dynamiquement.
-        if (fluid == Fluids.LAVA) return Identifier.withDefaultNamespace("textures/block/lava_still.png");
-        if (fluid == Fluids.WATER) return Identifier.withDefaultNamespace("textures/block/water_still.png");
-        return Identifier.withDefaultNamespace("textures/item/empty_armor_slot_shield.png"); // Fallback
+        if (fluid == Fluids.LAVA) return Identifier.withDefaultNamespace("textures/block/exposed_copper.png");
+        if (fluid == Fluids.WATER) return Identifier.withDefaultNamespace("textures/block/exposed_copper.png");
+        return Identifier.withDefaultNamespace("textures/block/exposed_copper.png"); // Fallback
     }
 
     @Override
@@ -92,7 +93,7 @@ public class CopperBucketSpecialRenderer implements SpecialModelRenderer<Fluid> 
         }
 
         public CopperBucketSpecialRenderer bake(final SpecialModelRenderer.BakingContext context) {
-            return new CopperBucketSpecialRenderer(new CopperBucketModel(context.entityModelSet().bakeLayer(new ModelLayerLocation(SCId.of("copper_bucket"), "main"))));
+            return new CopperBucketSpecialRenderer(new CopperBucketModel(context.entityModelSet().bakeLayer(COPPER_BUCKET_MODEL_LAYER)));
         }
     }
 }
